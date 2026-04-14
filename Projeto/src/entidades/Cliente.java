2537813c7728dbfc527620679f3bb812f4278c33
MrRafilds <rafaelbento.tieghisampaio@gmail.com>
2026-04-14 12:39:55 -0300

    add CadastroCliente

---
+++ Projeto/src/entidades/Cliente.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author mrrafilds
+ */
+public class Cliente {
+    
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String telefone;
+
+    public Cliente() {}
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    @Override
+    public String toString() {
+        return nome + " - " + cpf;
+    }
+}
---
+++ Projeto/src/entidades/Usuario.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author mrrafilds
+ */
+public class Usuario {
+    
+    private String login;
+    private String senha;
+    private String perfil;
+
+    public Usuario() {}
+
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+
+    public String getPerfil() { return perfil; }
+    public void setPerfil(String perfil) { this.perfil = perfil; }
+
+    @Override
+    public String toString() {
+        return login + " - " + perfil;
+    }
+    
+}
